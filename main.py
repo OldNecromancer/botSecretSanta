cdbd882852ac254fabb169098cb122e941716051
OldNecromancer <dushak01@mail.ru>
2024-11-16 10:23:22 +0300

    - adding help_me and hello commands

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+intents = discord.Intents.default()
+intents.messages = True
+intents.reactions = True
+intents.members = True
+intents.message_content = True
+
+load_dotenv()
 DISCORD_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
+# Просто в консоль для теста
 @bot.event
 async def on_ready():
     print("Готов к труду и обороне!")
 
-bot.run("DISCORD_TOKEN")
+
+# Тестовая команда
+# .hello
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Привет! Я твой супер-секретный Санта!')
+
+
+# Отображаем список всех доступных команд
+# .hello, .help, ...
+@bot.command()
+async def help_me(ctx):
+    await ctx.send('Тебе нужна помощь с командами? Вот список: ')
+
+
+bot.run(DISCORD_TOKEN)
